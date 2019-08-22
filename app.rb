0ef354bae2e81703c19755b34f962134db18c7c8
Nicole Dow <nikki.dow71@gmail.com>
2019-08-22 01:41:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello, World!"
+  end
+  
+  get '/name' do 
+    "My name is Nikki."
+  end
+  
+  get '/hometown' do 
+    "My hometown is Bradford."
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Back to Black."
+  end
+  
+  
+  
 end
